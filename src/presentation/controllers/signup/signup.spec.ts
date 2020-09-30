523676d133a15096899c396e21c65254829bb445
Milena Matos <milena.siqueira@agrotools.com.br>
2020-09-30 09:52:23 -0300

    test: ensure SignUpController returns 200 if valid data is provided

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -203,4 +203,24 @@ describe('SignUp Controller', () => {
       password: 'any_pass'
     })
   })
+
+  test('Should return 200 if valid data is provided', () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        name: 'valid',
+        email: 'valid@email.com',
+        password: 'valid_pass',
+        passwordConfirmation: 'valid_pass'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(200)
+    expect(httpResponse.body).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid@email.com',
+      password: 'valid_pass'
+    })
+  })
 })
