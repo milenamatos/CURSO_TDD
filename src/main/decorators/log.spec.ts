a226bce29ffa01b6e900b1ccb57ec15825840499
Milena Matos <milena.siqueira@agrotools.com.br>
2020-11-18 17:21:57 -0300

    test: ensure LogControllerDecorator returns the same result of the controller

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -45,4 +45,23 @@ describe('LogController Decorator', () => {
     await sut.handle(httpRequest)
     expect(handleSpy).toHaveBeenCalledWith(httpRequest)
   })
+
+  test('Should return the same result of the controller', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        email: 'milena@teste.com',
+        name: 'Milena',
+        password: 'password',
+        passwordConfirmation: 'password'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual({
+      statusCode: 200,
+      body: {
+        name: 'Milena'
+      }
+    })
+  })
 })
